14f72d8d914f9f192f9675a26d65b69d43a18c08
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-03 12:28:31 -0400

    BCPPP: S11. Functions: L103. Function Definition

--- Section 11/functions.cpp
+++ Section 11/functions.cpp
@@ -1,6 +1,47 @@
 /*
     Defining Functions:
         returntype name(parameter list) {
-            body
+            statement(s);
+            return value;
         }
 */
+
+#include <iostream>
+#define _USE_MATH_DEFINES // Needs to be before cmath
+#include <cmath>
+
+using namespace std;
+
+double calc_area_circle(double radius) {
+    return M_PI * radius * radius;
+}
+
+double calc_volume_cylinder(double radius, double height) {
+    return calc_area_circle(radius)  * height;   
+}
+
+void area_circle() {
+    double radius {};
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    cout << "The area of a circle with a radius " << radius << " is " << calc_area_circle(radius) << endl;     
+}
+
+void volume_cylinder() {
+    double radius {};
+    double height {};
+    cout << "Enter the radius of the cylinder: ";
+    cin >> radius;
+    cout << "Enter the height of the cylinder: ";
+    cin >> height;
+
+    cout << "The area of a cylinder with a radius " << radius << " and height " << height << " is " << calc_volume_cylinder(radius, height) << endl;
+}
+
+int main() {
+    area_circle();
+    volume_cylinder();
+
+    return 0;
+}
