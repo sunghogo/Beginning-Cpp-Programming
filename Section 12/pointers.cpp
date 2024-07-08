cb12129e61e1c3a99332df435a31c061a985e30a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-08 12:53:29 -0400

    BCPPP: S12. Pointers and References: L117 & 118

---
+++ Section 12/pointers.cpp
@@ -0,0 +1,14 @@
+/*
+    Pointers:
+        - A variable who value is an address (of another var or func)
+        - Used to:
+            - Access data outside of scope
+            - Operate on arrays efficiently
+            - Allocate memory dynamically on heap or free store
+            - Polymorphism in OO
+            - Access specific memory address for embedded/systems app
+    
+    Declaring pointers:
+        variable_type *pointer_name {nullptr};
+        - Initialize to nullptr (C+11, points to 0)
+*/
