c87007d09215621e136e77aa5e33ad063d603935
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-10 12:57:29 -0400

    BCPPP: S12. Pointers and References: L128. What is a Reference?

---
+++ Section 12/references.cpp
@@ -0,0 +1,45 @@
+/*
+    References:
+        - An alias for a variable
+        - Must be initialized to a variable when declared
+            - Cannot be null
+            - Cannot be made to refer a different variable after init
+        - Similar to constant pointer that is automatically dereferenced
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    int num {100};
+    int &ref {num};
+
+    cout << "num: " << num << endl; // 100
+    cout << "ref: " << ref << endl; // 100
+
+    cout << "\n------------------------------" << endl;
+    num = 200;
+    cout << "num: " << num << endl; // 200
+    cout << "ref: " << ref << endl; // 200
+
+    cout << "\n------------------------------" << endl;
+    num = 300;
+    cout << "num: " << num << endl; // 300
+    cout << "ref: " << ref << endl; // 300
+
+    cout << "\n------------------------------" << endl;
+    vector<string> stooges {"Larry", "Moe", "Curly"};
+
+    for (auto str: stooges) str = "Funny"; // str is a COPY of each vector element
+    for (auto str: stooges) cout << str << " " << endl; // No change
+
+    cout << "\n------------------------------" << endl;
+    for (auto &str: stooges) str = "Funny"; // str is now a reference of each vector element
+    for (auto const &str: stooges) cout << str << " " << endl; // Elements changed
+
+    return 0;
+}
