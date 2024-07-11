bd84142c106e73067b8a2bc22672a181e6a10f8a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-11 13:10:02 -0400

    BCPPP: S13. OOP L135. What is OOP? Update

---
+++ Section 13/oop.cpp
@@ -0,0 +1,18 @@
+/*
+    Procedural Programming:
+        - Programs are a collection of functions, so Focus is on the processes
+        - Data is declared separately, and passed as arguments into functions
+    
+    Procedural Programming Limitations:
+        - Functions need to know the structure of the data so not adaptable to changes in data structure
+        - As programs scale, they become difficult to maintain, extend, and debug
+
+    Object-Oriented Programming:
+        - Centered around classes and objects
+        - Abstraction:
+            - Classes model higher level real-world domain entities
+        - Encapsulations:
+            - Objects contain data AND operations that work on that data
+            - Abstract Data Type (ADT)
+            - Information-hiding implementation-specific logic from users
+*/
