c9f2613dc18a3f5f8712ab068352a571c6c6971f
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-12 09:30:55 -0400

    BCPPP: S13. OOP L136. What are Classes and Objects?

---
+++ Section 13/classes_objects.cpp
@@ -0,0 +1,13 @@
+/*
+    Classes and Objects:
+        - Classes: Blueprint from which objects are created. Contains:
+            - User-defined data-type
+            - Attributes (data)
+            - Methods (functions)
+            - Hide data and methods
+            - Provide a public interface
+        - Objects: Represents a specific instance of a class.
+            - Can create numerous objects
+            - Each has its own identity
+            - Each can use the defined class methods 
+*/
