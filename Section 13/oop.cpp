d6f6c5f7b2bd53d70a070b378117d2895c358815
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-12 09:26:58 -0400

    BCPPP: S13. OOP L135. What is OOP?

--- Section 13/oop.cpp
+++ Section 13/oop.cpp
@@ -15,4 +15,19 @@
             - Objects contain data AND operations that work on that data
             - Abstract Data Type (ADT)
             - Information-hiding implementation-specific logic from users
+        - Reusability:
+            - Easier to reuse classes in other applications
+        - Inheritances:
+            - Can create new classes in terms of reusing existing classes
+            - Polymorphic classes
+        - Polymorphism:
+            - Different behavior depending on the data
+
+    Limitations:
+        - Not a panacea
+            - May not be suitable for certain problems
+            - Entities may not decompose neatly to a class
+        - Steeper learning curve
+        - More up-front design (to create good models and hoerarchies)
+        - Usually larger in size, slower, and more complex
 */
