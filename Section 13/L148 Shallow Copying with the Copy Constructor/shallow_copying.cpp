45082d56759857fb128ff24112d90076b7bd3970
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-18 14:44:00 -0400

    BCPPP: S13. OOP: L.148. Shallow Copying with Copy Constructor

---
+++ Section 13/L148 Shallow Copying with the Copy Constructor/shallow_copying.cpp
@@ -0,0 +1,56 @@
+/*
+    Shallow vs Deep Copying:
+        - Consider a class that contains a pointer as a data member that is initialized and released dynamically in the constructor and destructor 
+            - The pointer is copied NOT what it points to (shallow copy
+            - If the pointer is changed or released in either the source or copy object, it'll affect the other
+        - Shallow copy (memberwise copy) is the default behavior by the compiler-generated copy constructor
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Shallow{
+private:
+    int *data;
+public:
+    void set_data_value(int d) { *data = d; }
+    int get_data_value() { return *data; }
+    
+    // Constructor
+    Shallow(int d);
+    // Copy Constructor
+    Shallow(const Shallow &source);
+    // Destructor
+    ~Shallow();
+};
+
+Shallow::Shallow(int d) 
+    : data {new int} {
+        *data = d;
+}
+
+Shallow::Shallow(const Shallow &source) 
+    : data {source.data} {
+        cout << "Copy constructor - shallow copy" << endl;
+}
+
+Shallow::~Shallow() {
+    delete data;
+    cout << "Destructor freeing data" << endl;
+}
+
+void display_shallow(Shallow s) {
+    cout << s.get_data_value() << endl;
+}
+
+int main() {
+
+    Shallow obj1 {100};
+    display_shallow(obj1); // Frees up copy AND obj1 data pointers
+
+    Shallow obj2 {obj1};
+    obj2.set_data_value(1000); // Sets obj2 AND obj1 data pointers
+
+    return 0; // Crashes because trying to free up already released data pointer
+}
