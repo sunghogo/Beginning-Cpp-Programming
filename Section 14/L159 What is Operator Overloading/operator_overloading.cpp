a76d6d21a8076d2e6d8fd6a40817709fbc5bfe39
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-07-25 13:17:25 -0400

    BCPPP: S14: Operator Overloading: L159 What is Operator Overloading update

---
+++ Section 14/L159 What is Operator Overloading/operator_overloading.cpp
@@ -0,0 +1,23 @@
+/*
+    Operator Overlaoding:
+        - Using traditional operators with user-definted types
+        - Allow user defined types to behave similar to bulit-in types 
+        - Can make code more readable and writable
+        - Must be explicitly defined (except for assignment operator)
+
+        - What operaters can be overloaded?
+            - The majority of C++ operators can be overloaded except for these:
+                - scope resolution operator (::)
+                - conditional operator (:?)
+                - pointer to member operator (.*)
+                - dot operator (.)
+                - sizeof operator (sizeof)
+        
+        - Basic rules:
+            - Precedence and ASsociativity cannot be changed
+            - 'arity' cannot be changed (ex. unary, binary, etc)
+            - Can't overload operators for primiitve types (ex. int, double, etc.)
+            - Can't create new operators
+            - [], (), ->, = operators MUST be declared as member methods
+            - Other operators can be declared as member methods or  global functions
+*/
