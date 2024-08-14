80a81cb1a24859370e6bad151e399e6c986f27ec
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-14 12:14:14 -0400

    BCPPP: S15: Inheritance L178: Multiple Inheritance

---
+++ Section 15/L178 Multiple Inheritance/multiple_inheritance.cpp
@@ -0,0 +1,13 @@
+/*
+    Multiple Inheritance:
+        - Occurs when a derived class inherits from two or more Base classes at the same time
+            - The Base classes may belong to unrelated class hierarchies
+
+        class Derived_Class:
+            public Base_Class_1, public Base_Class_2 {
+                ...
+            };
+
+        - Some compelling use-cases but easily misused
+        - Can be very complex
+*/
